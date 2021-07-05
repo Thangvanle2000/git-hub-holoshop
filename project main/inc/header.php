23874feaceea1753c7e29cab4f94e6d403a67673
TienTienBear <dat2611gd@gmail.com>
2021-07-05 13:46:46 +0700

    update lại header

--- project main/inc/header.php
+++ project main/inc/header.php
@@ -15,7 +15,7 @@
 <body>
 <div name="header-LOGO" style ="height: 100px; width: 100%">
   <div id ="logo"><a href="test.html" ><img src="images/main logo.png" alt="sample logo" ></a></div>
-  <div id ="headerLinks"><a href="Login-Register.html" title="Register-login">Register/Login</a><a href="#" title="Cart">Cart</a></div>
+  <div id ="headerLinks"><a href="Login-Register.php" title="Register-login">Register/Login</a><a href="#" title="Cart">Cart</a></div>
 </div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div class="container">
@@ -24,19 +24,19 @@
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="test.html">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="test.php">Home <span class="sr-only">(current)</span></a>
             </li>
-            
+			
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Computer</a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="asus.html">ASUS</a>
-                <a class="dropdown-item" href="macbook.html">Macbook</a>
-                <a class="dropdown-item" href="dell.html">DELL</a>
-				<a class="dropdown-item" href="hp.html">HP</a>
-				<a class="dropdown-item" href="acer.html">Acer</a>
+                <a class="dropdown-item" href="asus.php">ASUS</a>
+                <a class="dropdown-item" href="macbook.php">Macbook</a>
+                <a class="dropdown-item" href="dell.php">DELL</a>
+				<a class="dropdown-item" href="hp.php">HP</a>
+				<a class="dropdown-item" href="acer.php">Acer</a>
 				 
 				  
               </div>
@@ -51,12 +51,12 @@
             </li>
             <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Device&nbsp;</a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-				  <a class="dropdown-item" href="headphone.html">Headphone</a>
-				  <a class="dropdown-item" href="keyboard.html">Keyboard</a>
-                  <a class="dropdown-item" href="mouse.html">Mouse</a></div>
+				  <a class="dropdown-item" href="headphone.php">Headphone</a>
+				  <a class="dropdown-item" href="keyboard.php">Keyboard</a>
+                  <a class="dropdown-item" href="mouse.php">Mouse</a></div>
             </li>
             
-            <li class="nav-item"> <a class="nav-link" href="About us.html">About us</a></li>
+            <li class="nav-item"> <a class="nav-link" href="About us.php">About us</a></li>
 			  
           </ul>
           <form class="form-inline my-2 my-lg-0">

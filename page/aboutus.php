3a7c6a307608e00a089d33f2deb1c81ecf3e416c
Thangvanle2000 <70746075+Thangvanle2000@users.noreply.github.com>
2021-07-13 00:50:48 +0700

    fix about ú

--- page/aboutus.php
+++ page/aboutus.php
@@ -1,61 +1,65 @@
+<div name="content" class="col-9 ">
+	<div class="container mt-3">
+		<div class="row">
+			<div class="col-12">
+				<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 
-  <div name = "content" class="col-9 ">
-		<div class="container mt-3">
-      <div class="row">
-        <div class="col-12">
-          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-           
-        </div>
-      </div>
-      <hr>
-    </div>
-    <div class="container">
-     
-    </div>
-    <hr>
-  
-	<div class="container">
-		<div class="about-us">
-		<h2>The road to Holoshop</h2> 
-        <p>Holoshop was established since 2021. We sell cameras, computers and computer devices. 
-			Inspired by the early success of his dime store, and driven to bring even greater opportunity and value to his customers,
-		We want satisfy every people buy laptop or camera in Holoshop.
-		</p>
-		<p><img src="images/img-holoshop.jpg" width="800" height="400"></p><br>
-		<h2>How it works</h2>
-		<div class="how-work">
-		<div class="select-device">
-        <h4>Select Device</h4>
-		<br>
-		<img src="images/select-device.png" width="200" height="200">
-		<p>Start by searching your device above. You can also find it by category, brand and model.</p>
+				</div>
+			</div>
+			<hr>
 		</div>
-		<div class="give-detail">
-		<h4> Give us detail</h4><br>
-		<img src="images/give-detail.png" width="200" height="200">
-		<p>Answer a few questions about the computer and price you can buy. We will consult you the best price. </p>
-		</div>
-		<div class="ship-ping">
-        <h4>We will ship its</h4><br>
-		<img src="images/ship-ping.png" width="200" height="200">
-		<p>Print out your pre-paid shipping label, pack your device and shipping for you.</p>
-		</div>
-		</div>
-		<h2>Location store</h2>
-		<p>Find store at 150 Quitman, Wa. If you don't find it, please call us (+84) 0166-666-666. We alway wait you or you 
-		can shopping online. Our Website use easy and formal.If you need advice more, you can contact the website or go to 
-		the store directly. See you soon .
-		</p>
-		<img src="images/map-location.jpg" width="800" height="450">
-		<br>
-		<div class="free-ship">
-		<p>Holoshop launches two day:1/7-2/7 free-ship on more than 1000 item, no membership required. The program applies only to
-		shopping online	
-		</p>
-		<img src="images/img-freeship.jpg" width="800" height="400">
-		</div>
+		<div class="container">
 
+		</div>
+		<hr>
+
+		<div class="container">
+			<div class="about-us">
+				<h2>The road to Holoshop</h2>
+				<p>Holoshop was established since 2021. We sell cameras, computers and computer devices.
+					Inspired by the early success of his dime store, and driven to bring even greater opportunity and
+					value to his customers,
+					We want satisfy every people buy laptop or camera in Holoshop.
+				</p>
+				<p><img src="images/img-holoshop.jpg" style="width: 1000px; height: 450px;"></p><br>
+				<h2>How it works</h2>
+				<div class="how-work">
+					<div class="select-device">
+						<h4>Select Device</h4>
+						<br>
+						<img src="images/select-device.png" width="200" height="200">
+						<p>Start by searching your device above. You can also find it by category, brand and model.</p>
+					</div>
+					<div class="give-detail">
+						<h4> Give us detail</h4><br>
+						<img src="images/give-detail.png" width="200" height="200">
+						<p>Answer a few questions about the computer and price you can buy. We will consult you the best
+							price. </p>
+					</div>
+					<div class="ship-ping">
+						<h4>We will ship its</h4><br>
+						<img src="images/ship-ping.png" width="200" height="200">
+						<p>Print out your pre-paid shipping label, pack your device and shipping for you.</p>
+					</div>
+				</div>
+				<h2>Location store</h2>
+				<p>Find store at 150 Quitman, Wa. If you don't find it, please call us (+84) 0166-666-666. We alway wait
+					you or you
+					can shopping online. Our Website use easy and formal.If you need advice more, you can contact the
+					website or go to
+					the store directly. See you soon .
+				</p>
+				<img src="images/map-location.jpg" style="width: 1000px; height: 450px;">
+				<br>
+				<div class="free-ship">
+					<p>Holoshop launches two day:1/7-2/7 free-ship on more than 1000 item, no membership required. The
+						program applies only to
+						shopping online
+					</p>
+					<img src="images/img-freeship.jpg" style="width: 1000px; height: 400px;">
+				</div>
+
+			</div>
+		</div>
 	</div>
-	</div>	  
 </div>
-	</div>
